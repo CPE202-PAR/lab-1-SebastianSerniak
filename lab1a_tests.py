7d90cc6432694ac2f9290e6f4038f1b3e1c6703e
Sebastian Serniak <81578223+SebastianSerniak@users.noreply.github.com>
2021-04-08 01:08:43 -0700

    ok

--- lab1a_tests.py
+++ lab1a_tests.py
@@ -7,24 +7,43 @@ from lab1a import *
 class TestLab1(unittest.TestCase):
 
     def test_max_list_01(self) -> None:
-        tlist = [1,2,3]
-        self.assertEqual(max_list_iter(tlist),3)
+        tlist = [1, 2, 3]
+        self.assertEqual(max_list_iter(tlist), 3)   # basic test
 
     def test_max_list_02(self) -> None:
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
 
-    def test_reverse(self) -> None:
-        intlist = [1,2,3]
-        revlist = reverse_list(intlist)
-        self.assertEqual(revlist,[3,2,1])
-        self.assertEqual(intlist,[1,2,3])
+    def test_max_list_03(self) -> None:
+        tlist = []
+        self.assertEqual(max_list_iter(tlist), None)    # used to check for empty list
 
-    def test_reverse_mutate(self) -> None:
-        intlist = [1,2,3]
+    def test_reverse_01(self) -> None:
+        intlist = [1, 2, 3]
+        revlist = reverse_list(intlist)
+        self.assertEqual(revlist, [3, 2, 1])
+        self.assertEqual(intlist, [1, 2, 3])    # basic test
+
+    def test_reverse_02(self) -> None:
+        intlist = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            max_list_iter(intlist)
+
+    def test_reverse_mutate_01(self) -> None:
+        intlist = [1, 2, 3]
         reverse_list_mutate(intlist)
-        self.assertEqual(intlist,[3,2,1])
+        self.assertEqual(intlist, [3, 2, 1])    # used to check odd list length
+
+    def test_reverse_mutate_02(self) -> None:
+        intlist = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            max_list_iter(intlist)
+
+    def test_reverse_mutate_03(self) -> None:
+        intlist = [1, 2, 3, 4]
+        reverse_list_mutate(intlist)
+        self.assertEqual(intlist, [4, 3, 2, 1])   # used to check even list length
 
 if __name__ == "__main__":
         unittest.main()
