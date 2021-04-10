7f09dd399af45848570846f949dc96561f975b8e
Sebastian Serniak <81578223+SebastianSerniak@users.noreply.github.com>
2021-04-09 22:58:34 -0700

    Update location_tests.py

--- location_tests.py
+++ location_tests.py
@@ -5,10 +5,22 @@ from location import *
 
 class TestLocation(unittest.TestCase):
 
-    def test_repr(self) -> None:
+    def test_init(self) -> None:
         loc = Location('SLO', 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
+        self.assertEqual(repr(loc), "Location('SLO', 35.3, -120.7)")
+
+    def test_eq(self) -> None:
+        loc = Location('SLO', 35.3, -120.7)
+        loc1 = Location('SLO', 35.3, -120.7)
+        self.assertEqual(loc == loc1, True)
+
+    def test_repr0(self) -> None:
+        loc = Location('SLO', 35.3, -120.7)
+        self.assertEqual(repr(loc), "Location('SLO', 35.3, -120.7)")
+
+    def test_repr1(self) -> None:
+        loc = Location('Paris', 48.9, 2.4)
+        self.assertEqual(repr(loc), "Location('Paris', 48.9, 2.4)")
     # Add more tests!
 
 if __name__ == "__main__":
